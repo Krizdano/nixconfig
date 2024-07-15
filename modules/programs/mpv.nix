dfc19549d1ebb6011f7449d036a15b987dbf41f3
krizdano <opensauce.passover167@slmail.me>
2024-07-15 15:14:23 +0530

    added extra options

--- modules/programs/mpv.nix
+++ modules/programs/mpv.nix
@@ -1,9 +1,11 @@
-{ ... }: {
+{ pkgs, ... }: {
 
   programs.mpv = {
     enable = true;
     # package = pkgs.unstable.mpv;
     config = {
+      cache = "yes";
+      cache-secs = "120";
       hwdec = "auto-safe";
       gpu-context = "wayland";
       # ytdl-format= "bestvideo[height<=?1080]+bestaudio/best";
@@ -11,10 +13,10 @@
       sub-auto = "all";
     };
     defaultProfiles = [ "gpu-hq" ];
-    # scripts = with pkgs.mpvScripts; [
-    #  for mpv scripts
-    #  youtube-quality
-    # ];
+    scripts = with pkgs.mpvScripts; [
+      # for mpv scripts
+      quality-menu
+    ];
   };
 
 
