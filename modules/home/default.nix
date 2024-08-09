56a32195f7db78b56ba7b455f2a50ea4ff2a8d80
krizdano <opensauce.passover167@slmail.me>
2024-08-09 17:50:55 +0530

    fzf: changed colour scheme to catppuccin

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -11,16 +11,16 @@
     enable = true;
     enableZshIntegration = true;
     colors = {
-      fg = "#d0d0d0";
-      bg = "#121212";
-      hl = "#5f87af";
+      fg = "#cdd6f4";
+      bg = "#1e1e2e";
+      hl =  "#f38ba8";
       "fg+" = "#cdd6f4";
       "bg+" = "#313244";
       "hl+" = "#f38ba8";
       info = "#cba6f7";
       prompt = "#cba6f7";
       pointer = "#f5e0dc";
-      marker = "#7de60c";
+      marker = "#f5e0dc";
       spinner = "#f5e0dc";
       header = "#f38ba8";
     };
